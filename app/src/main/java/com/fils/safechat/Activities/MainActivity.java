8edc00640904eb1c4edaf28ac11b61a0f40817eb
alexinntekt <alex.inntekt@gmail.com>
2020-01-04 17:06:16 +0200

    am adaugat logout button in main

--- app/src/main/java/com/fils/safechat/Activities/MainActivity.java
+++ app/src/main/java/com/fils/safechat/Activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fils.safechat.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fils.safechat.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("running onCreate");
+
+//        logoutButton
+
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    protected void logout()
+    {
+        System.out.println("maia hi maia hu");
+        FirebaseAuth.getInstance().signOut();
+        Intent nextActivity = new Intent(getBaseContext(), SplashActivity.class);
+        nextActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(nextActivity);
+        finishAffinity();
     }
 }
