bcff833c061e91f7cc16a0eb3e6c51d61af14d3a
alexinntekt <alex.inntekt@gmail.com>
2020-01-04 18:15:10 +0200

    reading data from DB

--- app/src/main/java/com/fils/safechat/Activities/MainActivity.java
+++ app/src/main/java/com/fils/safechat/Activities/MainActivity.java
@@ -7,16 +7,33 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fils.safechat.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class MainActivity extends AppCompatActivity {
+
     ListView mMessageRecyclerView;
+    FirebaseDatabase database;
+    DatabaseReference myRefToDatabase;
+    ArrayList<String> messagesList = new ArrayList<String>();
+    JSONObject messages = null;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,15 +42,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("running onCreate");
 
-        mMessageRecyclerView = findViewById(R.id.messageListView);
-        ArrayList<String> a = new ArrayList<>();
-        a.add("1");
-        a.add("2");
-        a.add("3");
-        a.add("4");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messagesList);
         mMessageRecyclerView.setAdapter(adapter);
+
+        mMessageRecyclerView = findViewById(R.id.messageListView);
+        database = FirebaseDatabase.getInstance();
+        myRefToDatabase = database.getReference("messages");
+        myRefToDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    Gson gson = new Gson();
+                    String gsonString = gson.toJson(dataSnapshot.getValue());
+                    messagesList.clear();
+                    try {
+                        messages = new JSONObject(gsonString);
+                        Iterator<String> iterator = messages.keys();
+                        while (iterator.hasNext()) {
+                            String key = iterator.next();
+                            try {
+                                JSONObject message = new JSONObject(messages.get(key).toString());
+                                messagesList.add(message.get("text").toString());
+                            } catch (JSONException e) {
+                                // Something went wrong!
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
 //        logoutButton
 
